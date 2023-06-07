ac87b03a07cedbb6aebce6b57ea521336a41ab43
sansari <saifallah.m.ansari@gmail.com>
2023-06-07 19:51:11 -0400

    changed init map to .js and contact.html

--- src/path/contact/main.js
+++ src/path/contact/main.js
@@ -4,11 +4,3 @@ import waitForDom from 'js/wait-for-dom';
 waitForDom(() => {
   lazyload();
 });
-
-function initMap() {
-  var mapOptions = {
-    center: { lat: YOUR_LATITUDE, lng: YOUR_LONGITUDE },
-    zoom: YOUR_ZOOM_LEVEL
-  };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
